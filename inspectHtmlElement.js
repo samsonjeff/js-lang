43aceb0a3acf2761668ae71262a750daee4a979a
blurdev <159640757+samsonjeff@users.noreply.github.com>
2025-11-07 01:00:57 +0800

    Implement HTML element inspection in inspectHtmlElement.js
    Add functionality to inspect HTML elements.

---
+++ inspectHtmlElement.js
@@ -0,0 +1,8 @@
+const textarea = document.querySelector("textarea");
+const submitBtn = document.getElementById("submitBtn");
+const todoContainer = document.querySelector(".todoContainer");
+
+// way to inspect the html code
+console.log(todoContainer.innerHTML);
+console.log(submitBtn.innerHTML);
+console.log(textarea.innerHTML);
