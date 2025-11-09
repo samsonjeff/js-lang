a686ac27d2f54e8aa6d2be7009f9f3093eefe85c
blurdev <159640757+samsonjeff@users.noreply.github.com>
2025-11-09 13:43:42 +0800

    Update DOMmanipulation.js

--- DOMmanipulation.js
+++ DOMmanipulation.js
@@ -23,8 +23,9 @@
 // console.log(showDiv.innerText); // console: print out visble text on page
 
 const body = document.body;
-const h1 = document.createElement("h1");
-const strong = document.createElement("strong");
+const h1 = document.createElement("h1"); // create element
+const strong = document.createElement("strong"); // create element
 strong.textContent = "H1 si created and add text that show's on your page";
 h1.append(strong); // append strong to the element h1
 body.append(h1); // append the H1 to be visible
+
